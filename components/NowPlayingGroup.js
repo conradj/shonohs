4b311e413e6cef43125c0a56551d60ff1fb315f5
conradj <conradsjackson@gmail.com>
2020-06-01 20:50:50 +0100

    tidy up component

--- components/NowPlayingGroup.js
+++ components/NowPlayingGroup.js
@@ -4,20 +4,12 @@ import Card from "./base/Card";
 
 export default props => {
   const { data, error } = useSWR(`/api/groups/${props.groupId}`, fetch);
-
   if (error) return <div>failed to load</div>;
   if (!data) return <div>loading...</div>;
-
-  console.log("card", data.playbackMetadata);
 
   const {
     container: { name = "", imageUrl = "" }
   } = data.playbackMetadata;
-
-  if (data.playbackMetadata.container.imageUrl) {
-    console.log(data.playbackMetadata.container.imageUrl);
-  }
-
   return (
     <div>
       <Card imageUrl={imageUrl} name={name}>
