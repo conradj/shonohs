76bc87093b23e0101eebcef6f3d7cbc6329d83ac
conradj <conradsjackson@gmail.com>
2020-06-02 23:03:52 +0100

    web sockets on serverless using swr 🤷‍♂️

--- pages/api/events.js
+++ pages/api/events.js
@@ -1,12 +1,16 @@
+import { mutate } from "swr";
 import withSession from "../../lib/session";
 
 export default withSession(async (req, res) => {
   res.status(200);
+  let latestData = null;
   const headers = JSON.stringify(req.headers);
   //console.log(headers);
   const {
     "x-sonos-type": sonosEventType,
-    "x-sonos-namespace": sonosNameSpace
+    "x-sonos-namespace": sonosNameSpace,
+    "x-sonos-target-type": sonosTargetType,
+    "x-sonos-target-value": sonosTargetValue
   } = JSON.parse(JSON.stringify(req.headers));
 
   console.log("event posted", sonosNameSpace);
@@ -14,6 +18,10 @@ export default withSession(async (req, res) => {
     case "playbackmetadata":
       //const sendPlayBackToClientResponse = sendPlayBackToClient(req.body);
       console.info(`supported sonos event ${sonosNameSpace}`, req.body);
+      socket.on("data", data => {
+        latestData = data;
+        mutate(`/api/groups/${sonosTargetValue}`, data, false);
+      });
       break;
     default:
       console.error(`unsupported sonos event ${sonosNameSpace}`);
